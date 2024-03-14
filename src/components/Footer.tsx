c1c9b4ea3a1b58891064f916258ca53c4629b35c
Tomás Vidal <103294839+tomas-vidal@users.noreply.github.com>
2024-03-14 12:53:33 -0300

    changed footer's margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 function Footer() {
   return (
-    <footer className="text-center text-sm text-stone-700 my-5">
+    <footer className="text-center text-sm mt-8 text-stone-700 my-5">
       ©2024 Tomas Vidal. Todos los derechos reservados.
     </footer>
   );
